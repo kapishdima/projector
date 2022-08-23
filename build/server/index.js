5e3a2c1e74a007c029d75d7acd1da4da98104c69
DimaDevelopment <dima.development@fakel.com.ua>
2022-08-23 23:16:00 +0300

    create client basic structure

--- build/server/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _http_1 = require("@http/");
-const main = () => {
-    (0, _http_1.createServer)();
-};
-main();
